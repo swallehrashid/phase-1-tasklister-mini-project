562e50853a1577b8c0504e2d74fb30ab1b5c582b
swallehrashid <swallehmtawazo@gmail.com>
2025-10-04 14:04:24 +0300

    adds functionality to the to do list

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+    // your code here
+
+    let form = document.querySelector('form')
+      form.addEventListener('submit', (e) => {
+        e.preventDefault();
+        handleToDo(e.target['new-task-description'].value)
+        form.reset() 
+    }) 
+    
 });
+
+
+function handleToDo (toDo) {
+  let li = document.createElement('li');
+  let btn = document.createElement('button');
+  btn.addEventListener('click', handleDelete)
+  btn.textContent = ' X '
+  li.textContent = ` ${toDo} ` ;
+  li.appendChild(btn);
+  document.querySelector('#tasks').appendChild(li);
+
+}
+
+function handleDelete (e) {
+  e.target.parentNode.remove()
+} 
+
+
+
+
